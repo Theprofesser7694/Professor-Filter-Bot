7c9b5318af7da24efd7e404d82945308f6e9a89c
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-06-19 13:52:29 +0530

    Revert "temporary fix"
    This reverts commit d993a9f2c07bdc785777719ac1b46ac0ab24464f.

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ async def start():
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    # await NamanBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(temp.U_NAME, temp.B_NAME, today, time))
+    await NamanBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(temp.U_NAME, temp.B_NAME, today, time))
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
